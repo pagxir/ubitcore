4e92dbd00da1d1bc1b5e889b5552d86df6bc8384
pagxir <pagxir@gmail.com>
2008-06-19 16:19:27 +0000

    btcodec is finish.
    svn-id@34 gmail.com

--- nfast/btcodec.cc
+++ nfast/btcodec.cc
@@ -7,174 +7,261 @@
 #include <time.h>
 #include "btcodec.h"
 
-
-static const char *btcheck(const char *btbuf, const char *btend);
-
-static const char *btcheck_list(const char *btbuf, const char *btend)
-{
-    const char *btch = btbuf;
-    if (btch<btend && *btch=='l') {
-        btch++;
-        while (btch<btend && *btch != 'e') {
-            btch = btcheck(btch, btend);
-        }
-        return btch+1;
+#define TRY_KILL(en) \
+    if (en != &__INFINITE){ \
+        delete en; \
+        en = &__INFINITE; \
     }
-    return btend+1;
+
+class btlist: public bentity
+{
+    public:
+        btlist(btcodec *codec, int off);
+        const char *b_str(size_t *len);
+        virtual bentity& bget(int index);
+        ~btlist();
+
+    private:
+        bentity *b_entity;
+        btcodec *b_codec;
+        int b_off;
+};
+
+btlist::btlist(btcodec *codec, int off):
+    b_codec(codec), b_off(off), b_entity(&__INFINITE)
+{
+
 }
 
-static const char *btcheck_int(const char *btbuf, const char *btend)
+btlist::~btlist()
 {
-    int count = 0;
-    const char *btch = btbuf;
-    if (btch<btend && *btch=='i') {
-        btch++;
-        while (btch<btend && *btch!='e') {
-            count *= 10;
-            count += (*btch-'0');
-            btch++;
-        }
-        return btch+1;
-    }
-    return btend+1;
+    TRY_KILL(b_entity);
 }
 
-static const char *btcheck_str(const char *btbuf, const char *btend)
+const char *btlist::b_str(size_t *len)
 {
-    int count = 0;
-    const char *btch = btbuf;
-    while (btch<btend && isdigit(*btch)) {
-        count *= 10;
-        count += (*btch-'0');
-        btch++;
-    }
-    if (btch<btend && *btch!=':') {
-        return btend+1;
-    }
-    btbuf = btch + count + 1;
-    return btbuf;
-}
-
-static const char *btcheck_dict(const char *btbuf, const char *btend)
-{
-    const char *btch = btbuf;
-    if (btch<btend && *btch=='d') {
-        btch++;
-        while (btch<btend && *btch!='e') {
-            btch = btcheck_str(btch, btend);
-            btch = btcheck(btch, btend);
-        }
-        return btch+1;
-    }
-    return btend+1;
-}
-
-static const char *btcheck(const char *btbuf, const char *btend)
-{
-    if (btbuf<btend) {
-        switch (*btbuf) {
-        case 'd':
-            return btcheck_dict(btbuf, btend);
-        case 'l':
-            return btcheck_list(btbuf, btend);
-        case 'i':
-            return btcheck_int(btbuf, btend);
-        default:
-            if (isdigit(*btbuf)) {
-                return btcheck_str(btbuf, btend);
+    bentity* en = &__INFINITE;
+    const char *p = b_codec->b_str(b_off, len);
+    if (p != NULL){
+        size_t eln;
+        int offset = 1;
+        assert(p[0] == 'l');
+        while (offset<*len && p[offset]!='e'){
+            TRY_KILL(en);
+            en = b_codec->balloc(b_off+offset);
+            if (!en->b_str(&eln)){
+                break;
             }
+            offset += eln;
+        }
+        assert(p[offset] == 'e');
+        *len = offset+1;
+    }
+    TRY_KILL(en);
+    return p;
+}
+
+bentity& btlist::bget(int index)
+{
+    int i;
+    int off = 1;
+    size_t len;
+    bentity* en = &__INFINITE;
+    for (i=0; i<index; i++){
+        en = b_codec->balloc(b_off+off);
+        if (!en->b_str(&len)){
+            TRY_KILL(en);
+            return __INFINITE;
+        }
+        off += len;
+    }
+    TRY_KILL(b_entity);
+    b_entity = b_codec->balloc(off+b_off);
+    return *b_entity;
+}
+
+class btdict: public bentity
+{
+    public:
+        btdict(btcodec *codec, int off);
+        virtual const char *b_str(size_t *len);
+        virtual bentity& bget(const char *name);
+        ~btdict();
+
+    private:
+        bentity *b_entity;
+        btcodec *b_codec;
+        int b_off;
+};
+
+btdict::btdict(btcodec *codec, int off):
+    b_codec(codec), b_off(off), b_entity(&__INFINITE)
+{
+
+}
+
+btdict::~btdict()
+{
+    TRY_KILL(b_entity);
+}
+
+const char *btdict::b_str(size_t *len)
+{
+    bentity* en = &__INFINITE;
+    const char *p = b_codec->b_str(b_off, len);
+    if (p != NULL){
+        size_t eln;
+        int offset = 1;
+        assert(p[0] == 'd');
+        while (offset<*len && p[offset]!='e'){
+            TRY_KILL(en);
+            en = b_codec->balloc(b_off+offset);
+            if (!en->b_str(&eln)){
+                break;
+            }
+            offset += eln;
+        }
+        assert(p[offset] == 'e');
+        *len = offset+1;
+    }
+    TRY_KILL(en);
+    return p;
+}
+
+bentity& btdict::bget(const char *name)
+{
+    int i;
+    int off = 1;
+    size_t len = 0;
+    size_t eln = 0;
+
+    bentity* en =  b_codec->balloc(b_off+off);
+    while (en->b_str(&eln) != NULL){
+        off += eln;
+        const char *p = en->c_str(&len);
+        if (strlen(name)==len && 0==memcmp(name, p, len)){
+            TRY_KILL(en);
+            en = b_codec->balloc(b_off+off);
             break;
         }
-    }
-    return btend+1;
-}
-
-int btload_int(const char *btbuf, const char *btend)
-{
-    int count = 0;
-    if (btbuf<btend && *btbuf++!='i') {
-        return -1;
-    }
-    while (btbuf<btend && isdigit(*btbuf)) {
-        count *= 10;
-        count += (*btbuf-'0');
-        btbuf++;
-    }
-    if (btbuf<btend && *btbuf=='e') {
-        return count;
-    }
-    return -1;
-}
-
-const char *btload_str(const char *btbuf,
-                              const char *btend, int *plen)
-{
-    int count = 0;
-    while (btbuf<btend && isdigit(*btbuf)) {
-        count *= 10;
-        count += (*btbuf-'0');
-        btbuf++;
-    }
-    if (btbuf<btend && *btbuf==':') {
-        *plen = count;
-        return btbuf+1;
-    }
-    return btend+1;
-}
-
-const char *btstr_dup(const char *btstr, const char *btend)
-{
-    int len;
-    const char *str = btload_str(btstr, btend, &len);
-    if (str < btend) {
-        char *dp = new char[len+1];
-        memcpy(dp, str, len);
-        dp[len] = 0;
-        return dp;
-    }
-    return NULL;
-}
-
-const char *btload_list(const char *btbuf,
-                               const char *btend, int index)
-{
-    int count = 0;
-    const char *btch = btbuf;
-    if (btbuf<btend && *btch++=='l') {
-        while (*btch != 'e') {
-            if (count == index) {
-                return btch;
-            }
-            count++;
-            btch = btcheck(btch, btend);
+        TRY_KILL(en);
+        en = b_codec->balloc(b_off+off);
+        if (en->b_str(&eln) == NULL){
+            TRY_KILL(en);
+            break;
         }
+        off += eln;
+        TRY_KILL(en);
+        en = b_codec->balloc(b_off+off);
     }
-    return btend+1;
+    TRY_KILL(b_entity);
+    b_entity = en;
+    return *b_entity;
 }
 
-const char *btload_dict(const char *btbuf,
-                               const char *btend, const char *name)
+class btint: public bentity
 {
-    if (btbuf<btend && *btbuf++=='d') {
-        while (*btbuf != 'e') {
-            if (0==strncmp(name, btbuf, strlen(name))) {
-                return btbuf+strlen(name);
-            }
-            btbuf = btcheck_str(btbuf, btend);
-            btbuf = btcheck(btbuf, btend);
+    public:
+        btint(btcodec *codec, int off);
+        virtual int bget(int *ival);
+        virtual const char *b_str(size_t *len);
+
+    private:
+        btcodec *b_codec;
+        int b_off;
+};
+
+const char *btint::b_str(size_t *len)
+{
+    int off = 0;
+    const char *p = b_codec->b_str(b_off, len);
+    if (p != NULL){
+        assert(p[off]=='i');
+        while(off<*len && p[off]!='e'){
+            off++;
         }
+        assert(p[off]=='e');
+        *len = off+1;
     }
-    return btend+1;
+    return p;
 }
+
+int btint::bget(int *ival)
+{
+    size_t len;
+    *ival = atoi(b_codec->b_str(b_off+1, &len));
+    return 0;
+}
+
+btint::btint(btcodec *codec, int off):
+    b_codec(codec), b_off(off)
+{
+
+}
+
+class btstr: public bentity
+{
+    public:
+        btstr(btcodec *codec, int off);
+        virtual const char *b_str(size_t *len);
+        virtual const char *c_str(size_t *len);
+
+    private:
+        btcodec *b_codec;
+        int b_off;
+};
+
+btstr::btstr(btcodec *codec, int off):
+    b_codec(codec), b_off(off)
+{
+
+}
+
+const char *btstr::b_str(size_t *len)
+{
+    int off = 0;
+    const char *p = b_codec->b_str(b_off, len);
+    if (p != NULL){
+        assert(*len>3);
+        while (off<*len && p[off]!=':'){
+            off++;
+        }
+        assert(off<*len && p[off]==':');
+        int save = *len;
+        *len = atoi(p)+off+1;
+        assert(off+1+*len <= save);
+    }
+    return p;
+}
+
+const char *btstr::c_str(size_t *len)
+{
+    int off=0;
+    const char *p = b_str(len);
+
+    if (p != NULL){
+        while(p[off] != ':'){
+            off++;
+        }
+        p += (off+1);
+        *len -= (off+1);
+    }
+    return p;
+}
+
 
 btcodec::btcodec()
 {
     b_len = 0;
     b_text = NULL;
+    b_entity = &__INFINITE;
 }
 
 btcodec::~btcodec()
 {
+    if (b_entity != &__INFINITE){
+        delete b_entity;
+    }
     delete[] b_text;
 }
 
@@ -192,7 +279,43 @@ int btcodec::bload(const char *buffer, int len)
 
 bentity& btcodec::bget()
 {
-    return __INFINITE;
+    if (b_entity != &__INFINITE){
+        delete b_entity;
+        b_entity = &__INFINITE;
+    }
+    b_entity = balloc(0);
+    return *b_entity;
+}
+
+bentity* btcodec::balloc(int off)
+{
+    bentity *entity=&__INFINITE;
+    if (off < b_len){
+        entity = &__INFINITE;
+    }if (b_text[off] == 'l'){
+        entity = new btlist(this, off);
+    }else if (b_text[off] == 'd'){
+        entity = new btdict(this, off);
+    }else if (b_text[off] == 'i'){
+        entity = new btint(this, off);
+    }else if (isdigit(b_text[off])){
+        entity = new btstr(this, off);
+    }
+    return entity;
+}
+
+const char *btcodec::b_str(int off, size_t *len)
+{
+    if (off >= b_len){
+        return NULL;
+    }
+    *len = (b_len-off);
+    return b_text+off;
+}
+
+int bentity::bget(int *ival)
+{
+    return -1;
 }
 
 bentity& bentity::bget(int index)
@@ -205,7 +328,9 @@ bentity& bentity::bget(const char *name)
     return __INFINITE;
 }
 
-
+bentity::~bentity()
+{
+}
 
 const char *bentity::b_str(size_t *len)
 {
--- nfast/btcodec.h
+++ nfast/btcodec.h
@@ -3,6 +3,8 @@
 class bentity
 {
     public:
+        virtual ~bentity();
+        virtual int bget(int *ival);
         virtual bentity &bget(int index);
         virtual bentity &bget(const char *name);
         virtual const char *b_str(size_t *len);
@@ -15,11 +17,14 @@ class btcodec
         btcodec();
         ~btcodec();
         bentity &bget();
+        bentity *balloc(int off=0);
         int bload(const char *buffer, int len);
+        const char *b_str(int off, size_t *len);
 
     private:
         int b_len;
         char *b_text;
+        bentity *b_entity;
 };
 
 extern bentity __INFINITE;
