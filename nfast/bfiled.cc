af52279d74647942655ef246745c57325fc7fbe7
pagxir <pagxir@gmail.com>
2008-07-11 13:43:50 +0000

    svn-id@81 gmail.com

--- nfast/bfiled.cc
+++ nfast/bfiled.cc
@@ -52,7 +52,7 @@ bfiled::bdocall(time_t timeout)
     time_t now;
     time(&now);
 #if 0
-    printf("bcall(%s): %s\n", ident_text.c_str(), ctime(&now));
+	/* Should I use merge sort by now ? */
 #endif
     while(-1 != btime_wait(last_time+b_second)){
         last_time = time(NULL);
