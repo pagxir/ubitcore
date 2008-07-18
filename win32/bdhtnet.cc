822ff60b1ba8beb2fab94cbf371e56a89e0bc748
pagxir <pagxir@gmail.com>
2008-07-18 13:52:05 +0000

    DHT Finish.
    svn-id@91 gmail.com

--- win32/bdhtnet.cc
+++ win32/bdhtnet.cc
@@ -12,32 +12,33 @@
 #include "bsocket.h"
 #include "bdhtnet.h"
 #include "btcodec.h"
+#include "butils.h"
 
+#define DHT_TTL 3
 #define PF_PING  0x1000
 #define PF_FIND  0x2000
+#define PF_PEER  0x0100
+#define PF_QPEER  0x0200
+#define PF_QFIND  0x4000
+#define PF_QPING  0x8000
+
+#if 0
+#define get_peer_ident() __dht_id
+extern unsigned char __dht_id[];
+#endif
 
 unsigned char __ping_nodes[] = {
-  0x64, 0x31, 0x3a, 0x61, 0x64, 0x32, 0x3a, 0x69, 0x64, 0x32, 0x30, 0x3a,
-  0xd3, 0x86, 0x4b, 0x67, 0x3f, 0xc9, 0x47, 0xe1, 0xa8, 0xd8, 0x55, 0xd7,
-  0x00, 0x32, 0xbc, 0x44, 0x48, 0x56, 0x23, 0xaa, 0x65, 0x31, 0x3a, 0x71,
-  0x34, 0x3a, 0x70, 0x69, 0x6e, 0x67, 0x31, 0x3a, 0x74, 0x38, 0x3a, 0x51,
-  0x3b, 0x1b, 0x00, 0x01, 0x00, 0x00, 0x00, 0x31, 0x3a, 0x79, 0x31, 0x3a,
-  0x71, 0x65
+	"d1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t1:P1:y1:qe"
 };
 
 unsigned char __find_nodes[] = {
-  0x64, 0x31, 0x3a, 0x61, 0x64, 0x32, 0x3a, 0x69, 0x64, 0x32, 0x30, 0x3a,
-  0xd3, 0x86, 0x4b, 0x67, 0x3f, 0xc9, 0x47, 0xe1, 0xa8, 0xd8, 0x55, 0xd7,
-  0x00, 0x32, 0xbc, 0x44, 0x48, 0x56, 0x23, 0xaa, 0x36, 0x3a, 0x74, 0x61,
-  0x72, 0x67, 0x65, 0x74, 0x32, 0x30, 0x3a, 0xd3, 0x86, 0x48, 0x67, 0x3f,
-  0xc9, 0x47, 0xe1, 0xa8, 0xd8, 0x55, 0xd7, 0x00, 0x32, 0xbc, 0x44, 0x48,
-  0x56, 0x23, 0xab, 0x65, 0x31, 0x3a, 0x71, 0x39, 0x3a, 0x66, 0x69, 0x6e,
-  0x64, 0x5f, 0x6e, 0x6f, 0x64, 0x65, 0x31, 0x3a, 0x74, 0x38, 0x3a, 0x79,
-  0x3b, 0x1b, 0x00, 0x01, 0x00, 0x00, 0x00, 0x31, 0x3a, 0x79, 0x31, 0x3a,
-  0x71, 0x65
+    "d1:ad2:id20:abcdefghij01234567896:target20:abcdefghij0123456789e1:q9:find_node1:t1:F1:y1:qe"
 };
 
-static unsigned char __selfid[20];
+unsigned char __getpeers[] = {
+    "d1:ad2:id20:abcdefghij01234567899:info_hash20:mnopqrstuvwxyz123456e1:q9:get_peers1:t1:G1:y1:qe"
+};
+
 
 struct bdhtnode{
 	int            b_life;
@@ -52,9 +53,10 @@ struct bdhtnode{
 bool operator < (const bdhtnode& l, const bdhtnode &r)
 {
 	int i;
+    const unsigned char *selfid = get_peer_ident();
 	for (i=0; i<20; i++){
-         unsigned char lident = __selfid[i]^l.b_ident[i];
-         unsigned char rident = __selfid[i]^r.b_ident[i];
+         unsigned char lident = selfid[i]^l.b_ident[i];
+         unsigned char rident = selfid[i]^r.b_ident[i];
 		if (lident < rident){
 			return true;
 		}
@@ -64,6 +66,25 @@ bool operator < (const bdhtnode& l, const bdhtnode &r)
 	}
 	return false; 
 }
+
+struct peers_lesser{
+	bool operator()(const bdhtnode *l, const bdhtnode *r)
+	{
+	   	int i;
+        const unsigned char *info_hash = get_info_hash();
+	   	for (i=0; i<20; i++){
+		   	unsigned char lident = info_hash[i]^l->b_ident[i];
+		   	unsigned char rident = info_hash[i]^r->b_ident[i];
+		   	if (lident < rident){
+			   	return true;
+		   	}
+		   	if (lident > rident){
+			   	return false;
+		   	}
+	   	}
+	   	return false; 
+	}
+};
 
 struct ident_lesser{
 	bool operator()(const bdhtnode *l, const bdhtnode *r)
@@ -86,6 +107,7 @@ static std::queue<bdhtnode*> __out_nodes;
 static std::set<bdhtnode*, socket_lesser> __world_nodes;
 static std::set<bdhtnode*, socket_lesser> __ident_nodes;
 static std::set<bdhtnode*, ident_lesser>  __stack_find;
+static std::set<bdhtnode*, peers_lesser>  __stack_getpeers;
 
 class bdhtnet
 {
@@ -130,6 +152,22 @@ int bdhtnet_wrapper::bdocall(time_t timeout)
     return (__dhtnet.*b_call)(timeout);
 }
 
+static bdhtnode *__last_good_getpeer = NULL;
+static int
+active_get_peers(bdhtnode *node)
+{
+	peers_lesser lesser;
+	if (__last_good_getpeer == NULL){
+		__stack_getpeers.insert(node);
+		__play.bwakeup();
+	}else if (lesser.operator()(node, __last_good_getpeer)){
+		__stack_getpeers.insert(node);
+		__play.bwakeup();
+	}
+	node->b_flag |= PF_QPEER;
+	return 0;
+}
+
 
 static bdhtnode *__last_good_find = NULL;
 static int
@@ -142,6 +180,7 @@ active_find_node(bdhtnode *node)
 		__stack_find.insert(node);
 		__play.bwakeup();
 	}
+	node->b_flag |= PF_QFIND;
 	return 0;
 }
 
@@ -151,10 +190,10 @@ bdht_build(unsigned long host, unsigned short port,
 {
 	bdhtnode tnode, *build_node;
 	assert(elen==20);
-	tnode.b_life = 3;
+	tnode.b_life = DHT_TTL;
 	tnode.b_port = port;
 	tnode.b_host = host;
-	tnode.b_flag = PF_PING|PF_FIND;
+	tnode.b_flag = PF_PING|PF_FIND|PF_PEER;
 	memcpy(tnode.b_ident, id, 20);
 	if (__world_nodes.find(&tnode) == __world_nodes.end()){
 		build_node = new bdhtnode(tnode);
@@ -163,10 +202,20 @@ bdht_build(unsigned long host, unsigned short port,
 		build_node = *__world_nodes.find(&tnode);
 	}
 	if (build_node->b_flag&PF_FIND){
-	   	active_find_node(build_node);
+		if (0==(build_node->b_flag&PF_QFIND)){
+		   	active_find_node(build_node);
+		}
+	}
+	if (build_node->b_flag&PF_PEER){
+		if (0==(build_node->b_flag&PF_QPEER)){
+			active_get_peers(build_node);
+		}
 	}
 	if (build_node->b_flag&PF_PING){
-		__out_nodes.push(build_node);
+		if (0==(build_node->b_flag&PF_QPING)){
+		   	__out_nodes.push(build_node);
+			build_node->b_flag |= PF_QPING;
+		}
 	}
     return 0;
 }
@@ -182,8 +231,10 @@ bdht_decode_nodes(const char *nodes, int eln)
         memcpy(&port, nodes+i+24, 2);
         port = htons(port);
         bdht_build(host, port, nodes+i, 20);
+#if 0
         printf("add nodes: %s:%d\n",
                 inet_ntoa(*(in_addr*)&host), htons(port));
+#endif
     }
     assert(eln==i);
     return 0;
@@ -192,7 +243,21 @@ bdht_decode_nodes(const char *nodes, int eln)
 static int
 bdht_decode_peers(const char *values, int eln)
 {
+	printf("----------------------recv peers---------------------\n");
     return 0;
+}
+
+static int
+dump_find_node(bdhtnode *node, const char *title="find:")
+{
+	int i;
+
+	printf("%s ", title);
+	for (i=0; i<20; i++){
+		printf("%02x", node->b_ident[i]&0xff);
+	}
+	printf(" %s:%d %d\n", inet_ntoa(*(in_addr*)&node->b_host), node->b_port, node->b_life);
+	return 0;
 }
 
 int
@@ -218,6 +283,10 @@ bdhtnet::brecord(time_t timeout)
             printf("R: %c %c %s:%d\n", p1?*p1:'@', t2?*t2:'!',
                     inet_ntoa(*(in_addr*)&host), port);
 
+			if (p1!=NULL && *p1!='r'){
+				continue;
+			}
+
             const char *ident = codec.bget().bget("r").bget("id").c_str(&elen);
 
 			assert(elen == 20);
@@ -229,50 +298,59 @@ bdhtnet::brecord(time_t timeout)
 				assert(__world_nodes.find(id_node)
 						== __world_nodes.end());
 				__world_nodes.insert(id_node);
+				id_node->b_flag |= PF_PEER;
 				id_node->b_flag |= PF_FIND;
 				id_node->b_flag &= ~PF_PING;
-				id_node->b_life = 3;
-				active_find_node(id_node);
+				id_node->b_life = DHT_TTL;
+				if ((id_node->b_flag&PF_QFIND)==0){
+				   	active_find_node(id_node);
+				}
+				if ((id_node->b_flag&PF_QPEER)==0){
+				   	active_get_peers(id_node);
+				}
 			}else if (p1!=NULL && *p1=='r'){
 			    std::set<bdhtnode*, socket_lesser>::iterator nfind;
 			    nfind = __world_nodes.find(&tnode);
 				assert(nfind != __world_nodes.end());
 
-#if 0
+#if 1
 			   	const char *peers = codec.bget().bget("r").bget("values").c_str(&elen);
 			   	if (peers != NULL){
 				   	bdht_decode_peers(peers, elen);
-				   	bdht_build(host, port, ident, 20);
 				}
 #endif
 			   	const char *nodes = codec.bget().bget("r").bget("nodes").c_str(&elen);
 			   	if (nodes != NULL){
 					bdht_decode_nodes(nodes, elen);
-					(*nfind)->b_flag &= ~PF_FIND;
-					__last_good_find = *nfind;
 			   	}
+
+			   	const char *token = codec.bget().bget("r").bget("token").c_str(&elen);
+				if (token != NULL){
+					printf("get token!\n");
+					peers_lesser lesser;
+					if (__last_good_getpeer == NULL){
+					   	__last_good_getpeer = *nfind;
+					}else if (lesser.operator()(*nfind, __last_good_getpeer)){
+					   	__last_good_getpeer = *nfind;
+					}
+				   	(*nfind)->b_flag &= ~PF_PEER;
+				}else if (nodes != NULL){
+					if (__last_good_find == NULL){
+						__last_good_find = *nfind;
+					}else if (**nfind < *__last_good_find){
+						__last_good_find = *nfind;
+					}
+					__last_good_find = *nfind;
+				   	(*nfind)->b_flag &= ~PF_FIND;
+				}
+
+			   	dump_find_node(*nfind, "recved:");
 			   	(*nfind)->b_flag &= ~PF_PING;
-				(*nfind)->b_life = 3;
+				(*nfind)->b_life = DHT_TTL;
 			}
         }
     }
     return error;
-}
-
-static int
-dump_find_node(bdhtnode *node)
-{
-	int i;
-	for (i=0; i<20; i++){
-		printf("%02x", __selfid[i]&0xff);
-	}
-	printf("\n");
-	printf("find: ");
-	for (i=0; i<20; i++){
-		printf("%02x", node->b_ident[i]&0xff);
-	}
-	printf(" %s:%d %d\n", inet_ntoa(*(in_addr*)&node->b_host), node->b_port, node->b_life);
-	return 0;
 }
 
 int
@@ -284,6 +362,7 @@ bdhtnet::bplay(time_t timeout)
     unsigned long host = 0;
     unsigned short port = 0;
 	bdhtnode *find_node = NULL;
+	bdhtnode *get_peer_node = NULL;
 	while (!__stack_find.empty() && count<5){
 		std::set<bdhtnode*, ident_lesser>::iterator start;
 		start = __stack_find.begin();
@@ -293,7 +372,7 @@ bdhtnet::bplay(time_t timeout)
 		}
 		if ((*start)->b_life>0){
 			find_node = *start;
-		   	error = b_socket.bsendto(__find_nodes, sizeof(__find_nodes),
+		   	error = b_socket.bsendto(__find_nodes, sizeof(__find_nodes)-1,
 					(*start)->b_host, (*start)->b_port);
 			find_node->b_life--;
 			count ++;
@@ -302,25 +381,45 @@ bdhtnet::bplay(time_t timeout)
 		}
 		__stack_find.erase(start);
 	}
+	while (!__stack_getpeers.empty() && count<5){
+		std::set<bdhtnode*, peers_lesser>::iterator start;
+		start = __stack_getpeers.begin();
+        if (!((*start)->b_flag&PF_PEER)){
+			__stack_getpeers.clear();
+			break;
+		}
+		if ((*start)->b_life>=0){
+			get_peer_node = *start;
+		   	error = b_socket.bsendto(__getpeers, sizeof(__getpeers)-1,
+					(*start)->b_host, (*start)->b_port);
+			if (get_peer_node != find_node){
+			   	get_peer_node->b_life--;
+			}
+			count ++;
+			dump_find_node(*start, "getpeers:");
+			break;
+		}
+		__stack_getpeers.erase(start);
+	}
     while (!__out_nodes.empty() && count<5){
         bdhtnode *p = __out_nodes.front();
         if (p == marker){
             break;
         }
         __out_nodes.pop();
-	   	if (find_node==p){
-			printf("skip find node\n");
+	   	if (find_node==p || get_peer_node==p){
+			//printf("skip find node\n");
 	   	}else if (p->b_life == 0){
-			printf("node is dead!\n");
+			//printf("node is dead!\n");
 			continue;
 		}else if (p->b_flag&PF_PING){
 			p->b_life--;
 		   	error = b_socket.bsendto(__ping_nodes,
-				   	sizeof(__ping_nodes), p->b_host, p->b_port);
+				   	sizeof(__ping_nodes)-1, p->b_host, p->b_port);
 		   	assert(error != -1);
 		   	count ++;
 		}else {
-			printf("node remove from play list!\n");
+			//printf("node remove from play list!\n");
 			continue;
 		}
 		if (marker == NULL){
@@ -341,17 +440,17 @@ bdhtnet_node(const char *host, int port)
 {
 	static int __call_once_only = 0;
 	if (__call_once_only++ == 0){
-	   	btcodec codec;
-	   	size_t idl;
-	   	codec.bload((char*)__ping_nodes, sizeof(__ping_nodes));
-	   	const char *ident = codec.bget().bget("a").bget("id").c_str(&idl);
-		assert(idl == 20 && ident!=NULL);
-		memcpy(__selfid, ident, 20);
+		memcpy(__getpeers+12, get_peer_ident(), 20);
+		memcpy(__getpeers+46, get_info_hash(), 20);
+		memcpy(__ping_nodes+12, get_peer_ident(), 20);
+		memcpy(__find_nodes+12, get_peer_ident(), 20);
+		memcpy(__find_nodes+43, get_peer_ident(), 20);
+		__find_nodes[62]^=0x1;
 	}
 
 	bdhtnode *node = new bdhtnode();
-	node->b_life = 3;
-	node->b_flag = PF_PING;
+	node->b_life = DHT_TTL;
+	node->b_flag = PF_PING|PF_QPING;
 	node->b_host = inet_addr(host);
 	node->b_port = port;
 
