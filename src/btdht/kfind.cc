550b8c258348830cf2cb26f6f3de1ded5aa20938
pagxir <pagxir@gmail.com>
2008-11-04 09:57:38 +0000

    ddd
    git-svn-id: http://btload.googlecode.com/svn/branches/kadmin@173 gmail.com

--- src/btdht/kfind.cc
+++ src/btdht/kfind.cc
@@ -9,6 +9,9 @@
 #include "kbucket.h"
 #include "kfind.h"
 #include "knode.h"
+#include "bsocket.h"
+#include "btcodec.h"
+#include "provider.h"
 #include "transfer.h"
 
 struct kfind_arg{
@@ -36,6 +39,10 @@ kfind::vcall()
     int count;
     int error = 0;
     int state = b_state;
+    char buffer[8192];
+    in_addr_t host;
+    in_port_t port;
+    std::vector<kfind_arg*>::iterator iter;
     kitem_t nodes[_K];
 
     while (error != -1){
@@ -44,14 +51,51 @@ kfind::vcall()
             case 0:
                 count = get_knode(b_target, nodes, false);
                 if (count == -1){
-                    return -1;
+                    return 0;
+                }
+                for (i=0; i<count; i++){
+                    kfind_arg *arg = new kfind_arg;
+                    arg->host = nodes[i].host;
+                    arg->port = nodes[i].port;
+                    memcpy(arg->kadid, nodes[i].kadid, 20);
+                    kaddist_t dist(arg->kadid, b_target);
+                    b_kfind_queue.insert(
+                            std::make_pair(dist, arg));
                 }
                 break;
             case 1:
-                while (i<_K && b_concurrency<CONCURRENT_REQUEST){
+                while (b_concurrency<CONCURRENT_REQUEST){
+                    if (b_kfind_queue.empty()){
+                        break;
+                    }
+                    kfind_arg *arg = b_kfind_queue.begin()->second;
+                    arg->ship = b_net->get_kship();
+                    arg->ship->find_node(arg->host, arg->port,
+                            (uint8_t*)b_target);
+                    b_concurrency++;
                 }
                 break;
             case 2:
+                for (iter = b_kfind_out.begin(); 
+                        iter != b_kfind_out.end();
+                        iter++){
+                    if ((*iter)->ship == NULL){
+                        continue;
+                    }
+                    kship *ship = (*iter)->ship;
+                    count = ship->get_response(buffer,
+                                sizeof(buffer), &host, &port);
+                    if (count > 0){
+                        printf("Fould Packet!\n");
+                        delete ship;
+                        (*iter)->ship = NULL;
+                        state = 1;
+                    }
+                }
+                break;
+            case 3:
+                bthread::now_job()->tsleep(this, time(NULL)+34);
+                error = -1;
                 break;
             default:
                 break;
--- src/btdht/kfind.h
+++ src/btdht/kfind.h
@@ -1,6 +1,9 @@
 #ifndef __KFIND_H__
 #define __KFIND_H__
+#include "btkad.h"
+#include <vector>
 class bdhtnet;
+class kfind_arg;
 class kfind{
     public:
         kfind(bdhtnet *net, const char target[20]);
@@ -11,6 +14,8 @@ class kfind{
         int b_concurrency;
         int b_state;
         bdhtnet *b_net;
+        std::vector<kfind_arg*> b_kfind_out;
+        std::map<kaddist_t, kfind_arg*> b_kfind_queue;
 };
 int _find_node(char target[20]);
 #endif
