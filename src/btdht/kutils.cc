29dc5b11ef7c9f57c9dd80460c0a719b25fc9c03
pagxir <pagxir@gmail.com>
2008-11-04 13:09:43 +0000

    nothing to say
    git-svn-id: http://btload.googlecode.com/svn/branches/kadmin@174 gmail.com

---
+++ src/btdht/kutils.cc
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <netinet/in.h>
+
+#include "btkad.h"
+
+
+
+kaddist_t::kaddist_t()
+{
+    memset(kaddist, 0, 20);
+}
+
+kaddist_t::kaddist_t(const uint8_t kadid[20])
+{
+    memcpy(kaddist, kadid, 20);
+}
+
+kaddist_t::kaddist_t(const char kadid1[20], const char kadid2[20])
+{
+    int i;
+    for (i=0; i<20; i++){
+        kaddist[i] = kadid1[i]^kadid2[i];
+    }
+}
+
+bool kaddist_t::operator<(const kaddist_t &op2)const
+{
+    return memcmp(kaddist, op2.kaddist, 20)<0;
+}
+
+bool kaddist_t::operator>(const kaddist_t &op2)const
+{
+    return memcmp(kaddist, op2.kaddist, 20)>0;
+}
+
+bool kaddist_t::operator==(const kaddist_t &op2)const
+{
+    return memcmp(kaddist, op2.kaddist, 20)==0;
+}
+
+kaddist_t &kaddist_t::operator=(const kaddist_t &op)
+{
+    memcpy(kaddist, op.kaddist, 20);
+    return *this;
+}
+
+kadid_t::kadid_t()
+{
+    memset(kadid, 0, 20);
+}
+
+kadid_t::kadid_t(const char _kadid[20])
+{
+    memcpy(kadid, _kadid, 20);
+}
+
+kaddist_t kadid_t::operator^(const kadid_t &op2)const
+{
+    return kaddist_t(kadid, op2.kadid);
+}
+
+kadid_t & kadid_t::operator=(const kadid_t &op)
+{
+    memcpy(kadid, op.kadid, 20);
+    return *this;
+}
