9e4069d8e7694e68cea3036b107ee4ffbb037903
pagxir <pagxir@gmail.com>
2008-11-19 16:19:32 +0000

    DHT get peers
    git-svn-id: http://btload.googlecode.com/svn/branches/kadmin@273 gmail.com

--- src/btdht/kgetpeers.cc
+++ src/btdht/kgetpeers.cc
@@ -18,54 +18,62 @@
 #include "kutils.h"
 #include "transfer.h"
 
-kgetpeers::kgetpeers(bdhtnet *net, const char target[20],
-        kitem_t items[], size_t count)
+kgetpeers::kgetpeers(bdhtnet *net, const char target[20], kitem_t items[], size_t count)
 {
     int i;
-    b_net   = net;
     b_state = 0;
     b_trim  = false;
     b_sumumery = 0;
-    kgetpeers_t kfs;
     b_concurrency = 0;
-    b_last_concurrency = 0;
+    b_last_finding = 0;
+    b_last_update = time(NULL);
     memcpy(b_target, target, 20);
     for (i=0; i<count; i++){
-        kfs.item = items[i];
-        kfs.ship = NULL;
         kaddist_t dist(items[i].kadid, b_target);
         if (b_mapoutedkadid.insert(std::make_pair(dist, 1)).second){
-            if (b_mapoutedaddr.insert(std::make_pair(kfs.item.host, 1)).second){
-                b_qfind.insert(std::make_pair(dist, kfs));
+            if (b_mapoutedaddr.insert(std::make_pair(items[i].host, 1)).second){
+                b_qfind.insert(std::make_pair(dist, items[i]));
             }
         }
+        char buff[203];
+        sprintf(buff, "%s:%d\n",
+                inet_ntoa(*(in_addr*)&items[i].host),
+                htons(items[i].port));
+        b_loging +=  buff;
     }
+    b_ship = net->get_kship();
 }
+
+struct compat_t
+{
+    char ident[20];
+    char host[4];
+    char port[2];
+};
 
 void
 kgetpeers::kgetpeers_expand(const char buffer[], size_t count,
-        in_addr_t address, in_port_t port, const kitem_t *old)
+        in_addr_t address, in_port_t port)
 {
     size_t len;
+    kitem_t item;
     btcodec codec;
-    kgetpeers_t kfs;
     codec.bload(buffer, count);
 
     b_sumumery++;
-    kfs.ship = NULL;
     const char *vip = codec.bget().bget("r").bget("id").c_str(&len);
-    if (vip==NULL || len != 20){
-        failed_contact(old);
+    if (vip == NULL || len != 20){
         return;
     }
-    if (memcmp(vip, old->kadid, 20) != 0){
-        failed_contact(old);
-    }
+    b_loging += idstr(vip);
+    b_loging += "\n";
+    b_last_finding++;
     kaddist_t dist(vip, b_target);
-    kfs.item.port = port;
-    kfs.item.host = address;
-    memcpy(kfs.item.kadid, vip, 20);
-    update_contact(&kfs.item, true);
+    b_outqueue.erase(dist);
+    item.port = port;
+    item.host = address;
+    memcpy(item.kadid, vip, 20);
+    update_contact(&item, true);
     b_mapined.insert(std::make_pair(dist, 1));
     std::map<kaddist_t, int>::iterator backdist = b_mapined.end();
     backdist --;
@@ -76,38 +84,32 @@ kgetpeers::kgetpeers_expand(const char buffer[], size_t count,
         b_ended =  backdist->first;
         b_trim = true;
     }
-
-    typedef char compat_t[26];
-
     const char *compat = codec.bget().bget("r").bget("nodes").c_str(&len);
     if (compat != NULL && (len%26)==0){
         compat_t *iter, *compated = (compat_t*)(compat+len);
         for (iter=(compat_t*)compat; iter<compated; iter++){
-            kfs.item.host = *(in_addr_t*)&(*iter)[20];
-            kfs.item.port = *(in_port_t*)&(*iter)[24];
-            memcpy(kfs.item.kadid, &(*iter)[0], 20);
-            update_contact(&kfs.item, false);
-            dist = kaddist_t(kfs.item.kadid, b_target);
-            if (b_trim && b_ended<dist){
+            item.host = *(in_addr_t*)iter->host;
+            item.port = *(in_addr_t*)iter->port;
+            memcpy(item.kadid, iter->ident, 20);
+            update_contact(&item, false);
+            dist = kaddist_t(iter->ident, b_target);
+            if (b_trim && b_ended < dist){
                 continue;
             }
             if (b_mapoutedkadid.insert(std::make_pair(dist, 1)).second){
-                if (b_mapoutedaddr.insert(std::make_pair(kfs.item.host, 1)).second){
-                    b_qfind.insert(std::make_pair(dist, kfs));
+                if (b_mapoutedaddr.insert(std::make_pair(item.host, 1)).second){
+                    b_qfind.insert(std::make_pair(dist, item));
                 }
             }
         }
-        return;
     }
+
     int index = 0;
     typedef char peer_t[6];
     const char *peers = codec.bget().bget("r").bget("values").bget(index++).c_str(&len);
     while (peers!=NULL && (len%6)==0){
         peer_t *iter, *peered = (peer_t*)(peers+len);
         for (iter=(peer_t*)peers; iter<peered; iter++){
-            printf("peer(%2d): %s:%d\n", index,
-                    inet_ntoa(*(in_addr*)&(*iter)[0]),
-                    htons(*(in_port_t*)&(*iter)[4]));
         }
         peers = codec.bget().bget("r").bget("values").bget(index++).c_str(&len);
     }
@@ -138,64 +140,61 @@ kgetpeers::vcall()
                     if (b_trim && b_ended<b_qfind.begin()->first){
                         break;
                     }
-                    kgetpeers_t kfs = b_qfind.begin()->second;
+                    kitem_t item = b_qfind.begin()->second;
+                    b_outqueue.insert(*b_qfind.begin());
                     b_qfind.erase(b_qfind.begin());
-                    kfs.ship = b_net->get_kship();
-                    kfs.ship->get_peers(kfs.item.host, kfs.item.port,
-                            (uint8_t*)b_target);
-                    b_outqueue.push_back(kfs);
+                    b_ship->get_peers(item.host,
+                            item.port, (uint8_t*)b_target);
                     b_last_update = time(NULL);
-                    b_last_concurrency++;
+                    b_last_finding++;
                     b_concurrency++;
                 }
                 if (b_concurrency == 0){
-                    //printf("summery: %d\n", b_sumumery);
                     return b_sumumery;
                 }
-                delay_resume(b_last_update+5);
                 break;
             case 2:
                 error = -1;
-                for (int i=0; i<b_outqueue.size(); i++){
-                    if (b_outqueue[i].ship == NULL){
-                        continue;
-                    }
-                    kship *ship = b_outqueue[i].ship;
-                    count = ship->get_response(buffer,
-                            sizeof(buffer), &host, &port);
-                    if (count <= 0){
-                        continue;
-                    }
-                    kgetpeers_expand(buffer, count, host, port,
-                            &b_outqueue[i].item);
-                    b_outqueue[i].ship = NULL;
+                count = b_ship->get_response(buffer, sizeof(buffer), &host, &port);
+                while (count > 0){
+                    kgetpeers_expand(buffer, count, host, port);
+                    count = b_ship->get_response(buffer, sizeof(buffer), &host, &port);
                     b_concurrency--;
-                    delete ship;
                 }
                 thr = bthread::now_job();
-                if (thr->reset_timeout()){
-                    for (int i=0; i<b_outqueue.size(); i++){
-                        if (b_outqueue[i].ship != NULL){
-                            failed_contact(&b_outqueue[i].item);
-                            delete b_outqueue[i].ship;
-                            b_outqueue[i].ship = NULL;
-                        }
+                if (b_last_update+5<=now_time()){
+                    std::map<kaddist_t, kitem_t>::iterator iter;
+                    for (iter=b_outqueue.begin(); iter!=b_outqueue.end(); iter++){
+                        failed_contact(&iter->second);
                     }
-                    b_outqueue.resize(0);
+                    b_outqueue.clear();
                     b_concurrency = 0;
                 }
-                if (b_concurrency < CONCURRENT_REQUEST && b_last_concurrency){
-                    b_last_concurrency = 0;
+                if (b_concurrency<CONCURRENT_REQUEST && b_last_finding){
+                    b_last_finding = 0;
                     error = state = 0;
                 }else if (b_concurrency > 0){
+                    delay_resume(b_last_update+5);
                     thr->tsleep(this, "selecting");
-                }else {
+                }else{
                     return b_sumumery;
                 }
                 break;
             default:
+                assert(0);
                 break;
         }
     }
     return error;
+}
+
+kgetpeers::~kgetpeers()
+{
+    delete b_ship;
+}
+
+void
+kgetpeers::dump()
+{
+    printf("kgetpeers result: \n%s", b_loging.c_str());
 }
--- src/btdht/kgetpeers.h
+++ src/btdht/kgetpeers.h
@@ -1,40 +1,41 @@
-#ifndef __KGETPEERS_H__
-#define __KGETPEERS_H__
+#ifndef __KGET_PEERS_H__
+#define __KGET_PEERS_H__
 #include "btkad.h"
 #include "kutils.h"
 #include <vector>
+#include <string>
 class kship;
 class bdhtnet;
-
-struct kgetpeers_t{
-    kitem_t    item;
-    kship     *ship;
-};
 
 class kgetpeers{
     public:
         kgetpeers(bdhtnet *net, const char target[20],
                 kitem_t items[], size_t count);
-        void kgetpeers_expand(const char buffer[], size_t count,
-                in_addr_t host, in_port_t port, const kitem_t *old);
+        ~kgetpeers();
+        void dump();
+        void kgetpeers_expand(const char buffer[],
+                size_t count, in_addr_t host, in_port_t port);
         int vcall();
 
     private:
         int b_sumumery;
-        int b_last_concurrency;
+        int b_last_finding;
+        std::string b_loging;
 
     private:
         char b_target[20];
         int b_concurrency;
         int b_state;
-        bdhtnet *b_net;
-        time_t   b_last_update;
-        bool      b_trim;
-        kaddist_t b_ended;
 
     private:
-        std::vector<kgetpeers_t> b_outqueue;
-        std::map<kaddist_t, kgetpeers_t> b_qfind;
+        kaddist_t b_ended;
+        kship    *b_ship;
+        bool      b_trim;
+        time_t    b_last_update;
+
+    private:
+        std::map<kaddist_t, kitem_t> b_qfind;
+        std::map<kaddist_t, kitem_t> b_outqueue;
         std::map<kaddist_t, int> b_mapoutedkadid;
         std::map<in_addr_t, int> b_mapoutedaddr;
         std::map<kaddist_t, int> b_mapined;
